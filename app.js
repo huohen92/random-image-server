0d772ee7dd1ca6a7c237c5eec538924adec89962
julyeme <hze2928937824@163.com>
2026-03-22 18:22:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const app = express();
 const port = 5566;
 
-const VERSION = '0.02';
+const VERSION = '0.04';
 
 const SUPPORTED_EXT = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.svg', '.tiff', '.ico'];
 
